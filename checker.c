3b86cfe5aa9b5a3ba3c04b35e1b62226ee63668c
mohamedamine456 <aminelachheb456@gmail.com>
2021-03-17 10:32:09 +0100

    start the checker

--- checker.c
+++ checker.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 // ################################ Error ########################################### //
 
-void		fatal()
+void	fatal()
 {
 	write(1, "Error\n", 6);
 	exit(-1);
@@ -10,21 +11,49 @@ void		fatal()
 
 
 // ################################### Check ######################################## //
-int 
+
+int	ft_isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
+
+int		check_args(char **argv)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	while (argv[i] != NULL)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			if (!ft_isdigit(argv[i][j]))
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
 
 
-// ########################################################################### //
+// #################################### Main ####################################### //
 
-int main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
 	if (argc < 2)
-	{
-		write(1, "Error\n", 6);
-		return (0);
-	}
+		fatal();
 	else
 	{
-		if (check_args(&(args[1])))
-			write(1, "Error\n", 6);
+		if (check_args(&(argv[1])))
+			fatal();
+		else
+		{
+			write(1, "All Good", 8);
+			exit(0);
+		}
 	}
 }
