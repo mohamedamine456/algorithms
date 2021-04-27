fb1a5928f64a677e607ee1ac73c102df441dab08
mohamedamine456 <aminelachheb456@gmail.com>
2021-04-27 13:36:53 +0000

    ignore

--- stacks/push_swap.c
+++ stacks/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mlachheb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/27 07:53:12 by mlachheb          #+#    #+#             */
-/*   Updated: 2021/04/27 10:15:28 by mlachheb         ###   ########.fr       */
+/*   Updated: 2021/04/27 13:35:44 by mlachheb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int main(int argc, char **argv)
 {
 	//int		retv;
-	int		i = 0;
+	//int		i = 0;
 	t_stack	stack_a;
 	char	**operations;
 
@@ -30,12 +30,12 @@ int main(int argc, char **argv)
 		{
 			stack_a = fill_stack(argc, argv);
 			stack_a = sort_stack(stack_a, &operations);
-			while (i <= stack_a.top)
-				printf("%9ld", stack_a.items[i++]);
-			printf("\n");
-			i = 0;
-			while (operations != NULL && operations[i] != NULL)
-				printf("%9s", operations[i++]);
+			//while (i <= stack_a.top)
+			//	printf("%9ld", stack_a.items[i++]);
+			//printf("\n");
+			//i = 0;
+			//while (operations != NULL && operations[i] != NULL)
+			//	printf("%9s", operations[i++]);
 		}
 	}
 	return (0);
